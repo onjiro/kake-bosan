a441236510ebec981a42460fbd0ed8ddecfb24cd
onjiro <onjiro.mohyahya@gmail.com>
2013-05-19 11:03:31 +0900

    `rails new kakeibo-san`

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KakaiboSan::Application.config.session_store :cookie_store, key: '_kakaibo-san_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KakaiboSan::Application.config.session_store :active_record_store
