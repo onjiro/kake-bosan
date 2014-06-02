d2ffa09f3233cbc1734b4d446a9108e14f3c9d97
onjiro <onjiro.mohyahya@gmail.com>
2014-06-02 20:06:41 +0900

    AccountingTypes の5勘定科目分類を定数として定義

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -8,3 +9,10 @@
 Accounting::Side.delete_all()
 Accounting::Side::DEBIT.save()
 Accounting::Side::CREDIT.save()
+
+Accounting::Type.delete_all()
+Accounting::Type::ASSET.save()
+Accounting::Type::EXPENSE.save()
+Accounting::Type::LIABILITY.save()
+Accounting::Type::CAPITAL.save()
+Accounting::Type::INCOME.save()
