7856d1696accba8b2da0597e4269f5e801f07d09
onjiro <onjiro.mohyahya@gmail.com>
2014-07-12 23:49:00 +0900

    Re-set up Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter,
-  Settings.OmniAuth.twitter.consumer_key,
-  Settings.OmniAuth.twitter.consumer_secret,
-  display: 'popup'
+  provider :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret,
+  {
+    display: 'popup'
+  }
 end
