442f5301afdbb166d89ac7749da0f3576b1519ee
onjiro <onjiro.mohyahya@gmail.com>
2014-07-13 22:27:40 +0900

    Stop verify csrf token on OAuth callback api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_filter :authorize
+  skip_before_filter :authorize, :verify_authenticity_token
 
   def create
     auth = auth_params
