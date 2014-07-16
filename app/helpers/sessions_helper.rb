ca7ad3eaf6e63e7ad68b1f1bb7cab1494b6d066c
onjiro <onjiro.mohyahya@gmail.com>
2014-07-16 17:28:07 +0900

    アカウントの初期データをyamlで記述するように変更

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,58 +12,50 @@ module SessionsHelper
   end
 
   def create_initial_asset_datas(user)
-    # TODO 初期データを yml ファイルか何かに外出しにしておきたい
-    {
-      Accounting::Type::ASSET.id => [
-                                     ['現金'      , '現金として所持している金額を表す科目'],
-                                     ['普通預金'  , '普通預金口座を表す科目。金融機関、口座別の科目を作成するのがオススメ'],
-                                     ['電子マネー', '電子マネー等を表す科目。種類別の科目を作成するのがオススメ'],
-                                     ['ポイント'  , '各店舗のポイントを表す科目。店舗別に作成するのがオススメ'],
-                                    ],
-      Accounting::Type::EXPENSE.id => [
-                                       ['食費'                        , ''],
-                                       ['日用品'                      , ''],
-                                       ['雑貨'                        , ''],
-                                       ['遊興費'                      , ''],
-                                       ['住宅費'                      , ''],
-                                       ['電気料金'                    , ''],
-                                       ['ガス料金'                    , ''],
-                                       ['水道料金'                    , ''],
-                                       ['通信費'                      , ''],
-                                       ['交通費'                      , ''],
-                                       ['医療費'                      , ''],
-                                       ['被服費'                      , ''],
-                                       ['教育費'                      , ''],
-                                       ['交際費'                      , '祝儀・お見舞い・慶弔費（香典など）・会食代など'],
-                                       ['生命保険・医療保険'          , ''],
-                                       ['自動車保険'                  , ''],
-                                       ['健康保険'                    , ''],
-                                       ['国民年金・厚生年金・共済年金', ''],
-                                       ['雇用保険'                    , ''],
-                                       ['所得税'                      , ''],
-                                       ['住民税'                      , ''],
-                                       ['固定資産税'                  , ''],
-                                       ['手数料'                      , ''],
-                                       ['その他費用'                  , ''],
-                                      ],
-      Accounting::Type::LIABILITY.id => [
-                                         ['カード決済', ''],
-                                         ['借金'      , ''],
-                                        ],
-      Accounting::Type::CAPITAL.id => [
-                                      ],
-      Accounting::Type::INCOME.id => [
-                                      ['給与収入'    , ''],
-                                      ['その他雑収入', ''],
-                                     ],
-    }.each_pair do |asset_type_id, asset_items|
-      asset_items.each do |name, description|
-        Accounting::Item.new({
-                               user_id: user.id,
-                               name: name,
-                               accounting_type_id: asset_type_id,
-                               description: description,
-                             }).save()
+    initial_assets = YAML.load(ERB.new(<<-EOD).result)
+      <%= Accounting::Type::ASSET.id %>:
+        - { name: 現金         , description: 現金として所持している金額を表す科目 }
+        - { name: 普通預金     , description: 普通預金口座を表す科目。金融機関、口座別の科目を作成するのがオススメ }
+        - { name: 電子マネー   , description: 電子マネー等を表す科目。種類別の科目を作成するのがオススメ }
+        - { name: ポイント     , description: 各店舗のポイントを表す科目。店舗別に作成するのがオススメ }
+      <%= Accounting::Type::EXPENSE.id %>:
+        - { name: 食費         , description: ひみつ }
+        - { name: 日用品       , description: ひみつ }
+        - { name: 雑貨         , description: ひみつ }
+        - { name: 遊興費       , description: ひみつ }
+        - { name: 住宅費       , description: ひみつ }
+        - { name: 電気料金     , description: ひみつ }
+        - { name: ガス料金     , description: ひみつ }
+        - { name: 水道料金     , description: ひみつ }
+        - { name: 通信費       , description: ひみつ }
+        - { name: 交通費       , description: ひみつ }
+        - { name: 医療費       , description: ひみつ }
+        - { name: 被服費       , description: ひみつ }
+        - { name: 教育費       , description: ひみつ }
+        - { name: 交際費       , description: 祝儀・お見舞い・慶弔費（香典など）・会食代など }
+        - { name: 生命保険・医療保険
+                               , description: ひみつ }
+        - { name: 自動車保険   , description: ひみつ }
+        - { name: 健康保険     , description: ひみつ }
+        - { name: 国民年金・厚生年金・共済年金
+                               , description: ひみつ }
+        - { name: 雇用保険     , description: ひみつ }
+        - { name: 所得税       , description: ひみつ }
+        - { name: 住民税       , description: ひみつ }
+        - { name: 固定資産税   , description: ひみつ }
+        - { name: 手数料       , description: ひみつ }
+        - { name: その他費用   , description: ひみつ }
+      <%= Accounting::Type::LIABILITY.id %>:
+        - { name: カード決済   , description: ひみつ }
+        - { name: 借金         , description: ひみつ }
+      <%= Accounting::Type::CAPITAL.id %>: []
+      <%= Accounting::Type::INCOME.id %>:
+        - { name: 給与収入     , description: ひみつ }
+        - { name: その他雑収入 , description: ひみつ }
+    EOD
+    initial_assets.each_pair do |accounting_type_id, items|
+      items.each do |item|
+        Accounting::Item.new(item.merge(user_id: user.id, accounting_type_id: accounting_type_id)).save()
       end
     end
   end
