d239b87636dd0d59d008afd357fd33c809b27eb2
onjiro <onjiro.mohyahya@gmail.com>
2014-07-31 15:31:50 +0900

    /configsで勘定科目を表示

--- app/controllers/accounting/items_controller.rb
+++ app/controllers/accounting/items_controller.rb
@@ -2,10 +2,13 @@ class Accounting::ItemsController < ApplicationController
   # GET /accounting/items
   # GET /accounting/items.json
   def index
-    @accounting_items = Accounting::Item.where user_id: @current_user.id
+    @accounting_items = Accounting::Item
+      .where(user_id: @current_user.id)
+      .includes(:type)
+      .joins(:type)
 
     respond_to do |format|
-      format.json { render json: @accounting_items }
+      format.json { render json: @accounting_items, include: :type }
     end
   end
 end
