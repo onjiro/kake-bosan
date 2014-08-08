94730cd798562be882bf09e40f483d1267ca5288
onjiro <onjiro.mohyahya@gmail.com>
2014-08-08 23:06:08 +0900

    サーバー側で金額が設定されていないentryを弾くように実装

--- app/models/accounting/transaction.rb
+++ app/models/accounting/transaction.rb
@@ -2,4 +2,14 @@ class Accounting::Transaction < ActiveRecord::Base
   belongs_to :user
   has_many :entries, class_name: 'Accounting::Entry'
   accepts_nested_attributes_for :entries
+
+  before_save :remove_empty_entry, unless: 'entries.nil?'
+
+  private
+  def remove_empty_entry
+    return unless self.entries
+    self.entries.each do |entry|
+      entry.mark_for_destruction if entry.amount.nil? || entry.amount == 0
+    end
+  end
 end
