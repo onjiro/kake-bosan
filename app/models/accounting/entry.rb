dbc8d39ab5f12511ce6e4e8044180072dda7725b
onjiro <onjiro.mohyahya@gmail.com>
2014-08-15 18:06:24 +0900

    棚卸残高の照会SQLが誤っていたのを正しく修正

--- app/models/accounting/entry.rb
+++ app/models/accounting/entry.rb
@@ -7,11 +7,20 @@ class Accounting::Entry < ActiveRecord::Base
   # 指定日付以前のユーザーの棚卸額を科目ごとに集計します
   def self.inventories(user_id, date)
     e = Accounting::Entry.arel_table
-    t = Accounting::Transaction.arel_table
+    trx = Accounting::Transaction.arel_table
+    type = Accounting::Type.arel_table
+
     Accounting::Entry
-      .where(e[:user_id].eq(user_id).and(t[:date].lt(date)))
-      .joins(:transaction_belongs_to)
-      .select(e[:side_id], e[:item_id], e[:amount].sum.as('amount'))
-      .group([:side_id, :item_id])
+      .where(e[:user_id].eq(user_id).and(trx[:date].lt(date)))
+      .joins(:transaction_belongs_to, item: [:type])
+      .select(e[:item_id], type[:side_id], <<-EOD_AMOUNT)
+          SUM(
+            CASE WHEN accounting_entries.side_id = accounting_types.side_id
+              THEN  accounting_entries.amount
+              ELSE -accounting_entries.amount
+            END
+          ) AS amount
+        EOD_AMOUNT
+      .group(:item_id)
   end
 end
