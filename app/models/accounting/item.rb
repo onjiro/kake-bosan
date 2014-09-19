240d86b55df45a2df86d3992c978d75c71eb2574
onjiro <onjiro.mohyahya@gmail.com>
2014-09-19 13:22:32 +0900

    棚卸の反対科目取得を実装

--- app/models/accounting/item.rb
+++ app/models/accounting/item.rb
@@ -3,6 +3,11 @@ class Accounting::Item < ActiveRecord::Base
   belongs_to :type
 
   def self.inventory_fix_item_for(user_id, side)
-    return self.new(id: 0)
+    inventory_setting = InventorySetting
+      .where(user_id: user_id)
+      .includes(:debit_item, :credit_item)
+      .joins(:debit_item, :credit_item)
+      .first
+    return (side == Accounting::Side::CREDIT) ? inventory_setting.credit_item: inventory_setting.debit_item
   end
 end
