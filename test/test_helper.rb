4f8f1fb04d2c77c3cb45a1190bc3fd85c8cfdb7a
onjiro <onjiro.mohyahya@gmail.com>
2022-06-05 10:37:37 +0900

    `bin/rails test:all` が起動するようになった

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
-ENV['RAILS_ENV'] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require 'capybara/rails'
+require "capybara/rails"
 
 Capybara.default_driver = :selenium
-Capybara.default_wait_time = 20
+Capybara.default_max_wait_time = 20
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -20,37 +20,24 @@ class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
 
-  # Make sure not rollback on finish every case
-  # Change from webkit will be commited in any case.
-  # So, we clean table with database rewinder.
-  self.use_transactional_fixtures = false
   load "#{Rails.root}/db/seeds.rb"
 
-  def teardown_with_global
-    teardown_without_global
-    DatabaseRewinder.clean_with :truncate, except:
-      [
-       'accounting_sides',
-       'accounting_types',
-      ]
-  end
-  alias_method_chain :teardown, :global
-
   private
-  def sign_in(user = 'user', email = '')
+
+  def sign_in(user = "user", email = "")
     OmniAuth.config.mock_auth[:development] = OmniAuth::AuthHash.new
     {
-      provider: 'development',
-      uid: 'test_mock_user',
+      provider: "development",
+      uid: "test_mock_user",
       info: {
-        name:  user,
-        email: email
-      }
+        name: user,
+        email: email,
+      },
     }
-    visit '/auth/developer'
+    visit "/auth/developer"
   end
 
   def sign_out()
-    visit '/sessions/destroy'
+    visit "/sessions/destroy"
   end
 end
